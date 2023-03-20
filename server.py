6f21f750b38fa5d568ec30ef00d24443d8f3d963
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2023-03-20 12:09:45 +0000

    login

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request #bisogna importare la libreria request per poter fare richieste al server
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("form.html")
 
-@app.route('/controlla', methods=['GET'])
+@app.route('/controlla')
 def controlla():
     return ("<h1>Pagina di controllo</h1>")
+
+@app.route('/login')
+def login():
+    nomeUtente = request.args.get('nome')
+    passwordUtente = request.args.get('password')
+    if nomeUtente == "admin" and passwordUtente == "xxx123#":
+        return render_template("login.html", nome = nomeUtente, password = passwordUtente)
+    else:
+        return render_template("errore.html", password = passwordUtente)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
