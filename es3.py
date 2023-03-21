3458efbe854ae92ef5ac64fd9fa49a21aae07d79
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2023-03-21 10:42:23 +0000

    esercizio 3 completo

--- es3.py
+++ es3.py
@@ -6,24 +6,12 @@ app = Flask(__name__)
 def home():
     return render_template("es3.html")
 
-numeroHobby = 0
+
 @app.route('/login', methods = ['GET'])
 def login():
-    n1 = request.args.get("calcio")
-    n2 = request.args.get("playstation")
-    n3 = request.args.get("chitarra")
-    global numeroHobby
-    if n1 == None and n2 == None and n3 == None:
-        return render_template("login3.html", nHobby = 0, hobbies = "Nulla")
-    elif n1 != None:
-        return render_template("login3.html", nHobby = 1, hobbies = n1)
-    elif n2 != None:
-        return render_template("login3.html", nHobby = 1, hobbies = n2)
-    elif n3 != None:
-        return render_template("login3.html", nHobby = 1, hobbies = n3) 
-    
-    if n1 == "Calcio" and n2 == "Playstation" and n3 == "Chitarra":
-        return render_template("login3.html", nHobby = 3, hobbies = n3) 
+    hobbies = request.args.getlist("h")
+    numeroHobby = len(hobbies)
+    return render_template("login3.html", nHobby = numeroHobby, hobbies = hobbies) 
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
